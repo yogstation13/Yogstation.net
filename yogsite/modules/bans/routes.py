b4e82549b2ed09b880ef58f6907e3563003705f5
qwerty <qwertytrogi@gmail.com>
2020-07-08 13:27:59 -0500

    actually paginate the bans page, pagination macro

--- yogsite/modules/bans/routes.py
+++ yogsite/modules/bans/routes.py
@@ -1,8 +1,21 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
+
+import math
+
+from yogsite.config import cfg
+from yogsite import db
+
 
 blueprint = Blueprint("bans", __name__)
 
 @blueprint.route("/bans")
 def page_bans():
-	return render_template("bans.html")
+	page = request.args.get('page', type=int, default=1)
+
+	bans = db.game_db.query(db.Ban).limit(cfg.items_per_page).offset((page - 1) * cfg.items_per_page)
+
+	page_count = math.ceil(db.game_db.query(db.Ban.id).count() / cfg.items_per_page) # Selecting only the id on a count is faster than selecting the entire row
+
+	return render_template("bans.html", bans=bans, page=page, page_count=page_count)
