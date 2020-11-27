88218aa09c197513a1f65fe040890ee110f003a5
qwerty <qwertytrogi@gmail.com>
2020-11-27 02:49:07 -0600

    remove debug prints lagging rancher

--- yogsite/modules/admin/activity_tracker.py
+++ yogsite/modules/admin/activity_tracker.py
@@ -7,10 +7,7 @@ class AdminActivityAnalytics():
 
 		self.connections = db.game_db.query(db.Connection).filter(db.Connection.ckey.in_([admin.ckey for admin in self.admins])).all()
 
-		print(self.connections)
-
 		self.week = [[0]*24 for i in range(7)]
-
 
 		for connection in self.connections:
 			print(connection, connection.duration().total_seconds())
@@ -19,20 +16,11 @@ class AdminActivityAnalytics():
 				start_hour = connection.datetime.hour
 				start_day = connection.datetime.weekday()
 
-				print(start_hour, start_day)
-
 				for hour in range(int(connection.duration().total_seconds() // 3600)):
 					marking_hour = (start_hour + hour) % 24 # Handle day rollover
 					marking_day = start_day + (start_hour+hour) // 24
-
-					print(hour, marking_hour, marking_day)
 
 					if marking_day >= 7: # If it's outside of the week we're looking at, WE DONT CARE HAHAHAH
 						break
 
 					self.week[marking_day][marking_hour] += 1
-
-		print(self.week)
-					
-				
-
