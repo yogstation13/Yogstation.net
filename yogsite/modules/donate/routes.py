ad8dd667ed38f50f5de14ff697f28ef4eaa6f307
qwerty <qwertytrogi@gmail.com>
2021-03-01 16:32:23 -0600

    verifying the paypal IPN

--- yogsite/modules/donate/routes.py
+++ yogsite/modules/donate/routes.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 
+import requests
+
 blueprint = Blueprint("donate", __name__)
 
 @blueprint.route("/donate")
@@ -11,5 +13,19 @@ def page_donate():
 
 @blueprint.route("/api/paypal_donate", methods=["GET", "POST"])
 def page_api_paypal_donate():
-	print(request.args, request.form, request.url)
+	args_string = request.full_path.replace(request.path, "").lstrip("?") # Get just the arguments portion of the url
+	
+	response_request_url = f"{cfg.get('paypal.ipn_url')}?cmd=_notify-validate&{args_string}"
+
+	print("Response Request Url:", response_request_url)
+
+	verification_request = requests.get(response_request_url, headers={"User-Agent": "IPN-VerificationScript"})
+
+	print(verification_request.body)
+
+	payment_amount = request.args.get("mc_gross", type=float)
+	ckey = request.args.get("custom")
+
+	print(payment_amount, ckey)
+
 	return ""
