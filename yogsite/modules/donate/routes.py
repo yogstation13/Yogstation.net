f1b11e73af02a038e88873412e2e0fe8681a084c
qwerty <qwertytrogi@gmail.com>
2021-03-01 13:50:28 -0600

    paypal donation framework??

--- yogsite/modules/donate/routes.py
+++ yogsite/modules/donate/routes.py
@@ -1,8 +1,15 @@
+from flask import abort
 from flask import Blueprint
 from flask import render_template
+from flask import request
 
 blueprint = Blueprint("donate", __name__)
 
 @blueprint.route("/donate")
 def page_donate():
 	return render_template("donate.html")
+
+@blueprint.route("/api/paypal_donate", methods=["GET", "POST"])
+def page_api_paypal_donate():
+	print(request.args, request.form)
+	return ""
