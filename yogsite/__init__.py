08b35ba1f06b6a310947a9b28812dac4310b23fb
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-06-25 19:15:09 -0400

    Update __init__.py

--- yogsite/__init__.py
+++ yogsite/__init__.py
@@ -34,7 +34,8 @@ def create_app():
 	app.secret_key = cfg.get("secret_key") # Used for signing sessions
 
 	app.config['SESSION_COOKIE_SECURE'] = True
-	app.config['SESSION_COOKIE_HTTPONLY'] = False
+	app.config['SESSION_EXPIRE_AT_BROWSER_CLOSE'] = False
+	app.config['SESSION_COOKIE_AGE'] = 1209600 
 	app.config['SESSION_COOKIE_SAMESITE'] = "None"
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://{username}:{password}@{host}:{port}/{db}".format(
