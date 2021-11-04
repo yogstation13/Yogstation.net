693d0a0b2e7f9c29bfc5862f17b3b3ec2de3d11a
Mordrehel <55900629+Mordrehel@users.noreply.github.com>
2021-11-03 20:49:35 -0500

    Doesn't Show LOAs more than a year in the future
    Title

--- yogsite/modules/admin/routes.py
+++ yogsite/modules/admin/routes.py
@@ -24,8 +24,11 @@ def page_loa():
 	form_set_loa = SetLOAForm(request.form, prefix="form_set_loa")
 
 	loas = db.game_db.query(db.LOA).filter(
-		db.LOA.expiry_time > (datetime.utcnow() - timedelta(days=30)).date()
-	).order_by(db.LOA.id.desc()) # Get LOAs sorted by start time
+                and_(
+                  db.LOA.expiry_time < (datetime.utcnow() + timedelta(years=1)).date(),
+                  db.LOA.expiry_time > (datetime.utcnow() - timedelta(days=30)).date()
+                )
+    ).order_by(db.LOA.id.desc()) # Get LOAs sorted by start time
 
 	if not g.current_user.has_perms("loa.others"):
 		loas = loas.filter(db.LOA.ckey == g.current_user.ckey)
