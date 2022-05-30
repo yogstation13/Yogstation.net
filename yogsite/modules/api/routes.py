a3120166ef18d2017b93777a791b623c0e87151f
Ashleigh Carr <ashcorr20@gmail.com>
2022-05-30 23:39:51 +0100

    Update routes.py

--- yogsite/modules/api/routes.py
+++ yogsite/modules/api/routes.py
@@ -7,11 +7,3 @@ from yogsite import db
 from yogsite.util import query_server_status
 
 blueprint = Blueprint("api", __name__)
-
-@blueprint.route("/api/stats/<string:server_id>")
-def page_api_stats_server_id(server_id):
-	if server_id not in cfg.get("servers"):
-		return jsonify({"error": "server id not found"})
-
-	server_stats = query_server_status(server_id)
-	return jsonify(server_stats)
